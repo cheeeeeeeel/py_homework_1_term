d995ab34a701b987245d0c2f9757e1444fa2a1b4
Rudakov Matvey <rudakov.matvey@yandex.ru>
2026-04-01 17:45:31 +0300

    Исправил chain итератор

--- 5_iterators/self_chain/self_chain.py
+++ 5_iterators/self_chain/self_chain.py
@@ -4,9 +4,24 @@ T = TypeVar("T")
 
 
 def chain(*iterables: Iterable[T]) -> Generator[T, None, None]:
-    """Пишите ваш код здесь"""
+    """Объединяет несколько итерируемых объектов в один последовательный итератор."""
+    for obj in iterables:
+        yield from obj
 
 
 class Chain:
     def __init__(self, *iterables: Iterable[T]):
-        """Реализуйте класс ниже"""
+        self._iterables = iter(iterables)
+        self._iter = None
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        while True:
+            if self._iter is None:
+                self._iter = iter(next(self._iterables))
+            try:
+                return next(self._iter)
+            except StopIteration:
+                self._iter = None
