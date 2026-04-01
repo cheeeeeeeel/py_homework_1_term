463a2e9247df0dcca2181af73992e235d614ea65
Rudakov Matvey <rudakov.matvey@yandex.ru>
2026-04-01 19:03:57 +0300

    Upgrade cycle итератор

--- 5_iterators/self_cycle/self_cycle.py
+++ 5_iterators/self_cycle/self_cycle.py
@@ -5,20 +5,36 @@ T = TypeVar("T")
 
 def cycle(obj: Iterable[T]) -> Generator[T, None, None]:
     """Бесконечно повторяет элементы заданной последовательности."""
-    while True:
-        yield from obj
+    saved = []
+    for elem in obj:
+        yield elem
+        saved.append(elem)
+
+    while saved:
+        yield from saved
+
 
 class Cycle:
     def __init__(self, obj: Iterable[T]):
-        self._obj = obj
-        self._iter = iter(obj)
+        self._obj = iter(obj)
+        self._iter = None
+        self._saved = []
+        self._ind = 0
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        try:
-            return next(self._iter)
-        except StopIteration:
-            self._iter = self._obj.__iter__()
-            return next(self._iter)
+        if self._obj:
+            try:
+                elem = next(self._obj)
+                self._saved.append(elem)
+                return elem
+            except StopIteration:
+                if not self._saved:
+                    raise
+                self._obj = None
+
+        elem = self._saved[self._ind]
+        self._ind = (self._ind + 1) % len(self._saved)
+        return elem
